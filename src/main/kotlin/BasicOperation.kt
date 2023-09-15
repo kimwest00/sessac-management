be0bded245aff599b9de3b6c8949196f194d5a41
kimwest00 <minseohufs@hufs.ac.kr>
2023-09-15 16:33:33 +0900

    feat:#2 add inteface / management, operation

---
+++ src/main/kotlin/BasicOperation.kt
@@ -0,0 +1,8 @@
+interface BasicOperation {
+    //TODO: 1.Create
+    fun <T>createData(data:T)
+    //TODO: 2.Update
+    fun <T>updateData(data:T)
+    //TODO: 3.Read
+    fun readData()
+}
---
+++ src/main/kotlin/Management.kt
@@ -0,0 +1,7 @@
+interface Management {
+    //TODO: 1.Add
+    fun <T>addObject(data:T)
+
+    //TODO: 2.Read
+    fun <T>readObject(data:T)
+}
