34671099fd0b70a72215a3a934604e4255bcccc7
lgy15 <lgy159@naver.com>
2023-09-16 11:50:22 +0900

    feat:#7 updateCompany 함수 구현 및 테스ㅡ

--- src/main/kotlin/data/CompanyManagement.kt
+++ src/main/kotlin/data/CompanyManagement.kt
@@ -6,6 +6,9 @@ class CompanyManagement {
     // 1. 회사 이름을 입력 받는다
     // 2. 회사 이름 기준 검색
     // 3. 객체를 찾으면 접근하여 속성 변환
+    fun addCompany(comp: CompanyData) {
+        compList.add(comp)
+    }
 
     fun searchCompany(name: String): ArrayList<Int> {
         val idList = ArrayList<Int>()
@@ -17,12 +20,18 @@ class CompanyManagement {
         return idList
     }
 
-    fun updateCompany(target: String, inputData: String) {
-        when (target) {
-            "name" -> compData.name = inputData
-            "address" -> compData.address = inputData
-            "CEO" -> compData.CEO = inputData
-            "phone" -> compData.phone = inputData
+
+    fun updateCompany(target: String, inputData: String, id: Int) {
+        for (it in compList) {
+            if (it.id == id) {
+                when (target) {
+                    "name" -> it.name = inputData
+                    "address" -> it.address = inputData
+                    "CEO" -> it.CEO = inputData
+                    "phone" -> it.phone = inputData
+                }
+                break
+            }
         }
     }
 }
