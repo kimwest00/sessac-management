bd1e5470d5b69fe26a50ab3e769d8d2aa48dec09
kimwest00 <minseohufs@hufs.ac.kr>
2023-09-16 12:03:11 +0900

    feat:#5 companyManagement logic 적용

--- src/main/kotlin/data/IdolManagement.kt
+++ src/main/kotlin/data/IdolManagement.kt
@@ -2,4 +2,33 @@ package data
 
 class IdolManagement {
     var idolList = ArrayList<IdolData>()
+
+    fun addIdol(idol: IdolData) {
+        idolList.add(idol)
+    }
+
+    fun searchIdol(name: String): ArrayList<Int> {
+        val idList = ArrayList<Int>()
+        idolList.forEach {
+            if (it.name == name) {
+                idList.add(it.id)
+            }
+        }
+        return idList
+    }
+
+
+    fun updateIdol(target: String, inputData: String, id: Int) {
+        for (it in idolList) {
+            if (it.id == id) {
+                when (target) {
+                    "name" -> it.name = inputData
+                    "companyName" -> it.companyName = inputData
+                    "gender" -> it.gender = inputData
+                    "song" -> it.song = inputData
+                }
+                break
+            }
+        }
+    }
 }
