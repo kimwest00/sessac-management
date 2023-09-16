6963a10cdfb121de5f55e85bdd9ea62eeeaf0ed2
lgy15 <lgy159@naver.com>
2023-09-16 14:27:40 +0900

    feat:#7 class management 수정

--- src/main/kotlin/data/EventManagement.kt
+++ src/main/kotlin/data/EventManagement.kt
@@ -2,47 +2,50 @@ package data
 
 import util.Generator
 
-class EventManagement {
-    fun addEvent(event: Event) {
-        val id = Generator.generateEmpNum()
-        try {
-            print("행사 이름 : ")
-            val name = readLine()!!
-            print("장소 : ")
-            val address = readLine()!!
-            print("content : ")
-            val content = readLine()!!
-            print("날짜 : ")
-            val date = readLine()!!
-            ObjectManagement.eventList.add(Event(id, name, address, content, date))
-        } catch (_: NullPointerException) {
-            println("잘못 입력")
-        }    }
+fun addEvent(event: Event) {
+    val id = Generator.generateEmpNum()
+    try {
+        print("행사 이름 : ")
+        val name = readLine()!!
+        print("장소 : ")
+        val address = readLine()!!
+        print("content : ")
+        val content = readLine()!!
+        print("날짜 : ")
+        val date = readLine()!!
+        ObjectManagement.eventList.add(Event(id, name, address, content, date))
+    } catch (_: NullPointerException) {
+        println("잘못 입력")
+    }    }
 
-    fun searchEvent(name: String): Event? {
-        ObjectManagement.eventList.forEach {
-            if (it.name == name) {
-                return it
-            }
-        }
-        return null
-    }
-
-
-    fun updateEvent(target: String, inputData: String, id: Int) {
-        for (it in ObjectManagement.eventList) {
-            if (it.id == id) {
-                when (target) {
-                    "name" -> it.name = inputData
-                    "address" -> it.address = inputData
-                    "content" -> it.content = inputData
-                    "date" -> it.date = inputData
-                }
-                break
-            }
+fun searchEvent(name: String): Event? {
+    ObjectManagement.eventList.forEach {
+        if (it.name == name) {
+            return it
         }
     }
+    return null
+}
 
 
+fun updateEvent(target: String, inputData: String, id: Int) {
+    for (it in ObjectManagement.eventList) {
+        if (it.id == id) {
+            when (target) {
+                "name" -> it.name = inputData
+                "address" -> it.address = inputData
+                "content" -> it.content = inputData
+                "date" -> it.date = inputData
+            }
+            break
+        }
+    }
+}
 
+fun readCompany(event: Event) {
+    for (field in event.javaClass.declaredFields) {
+        field.isAccessible = true
+        val value = field.get(event)
+        println("${field.name}: $value")
+    }
 }
