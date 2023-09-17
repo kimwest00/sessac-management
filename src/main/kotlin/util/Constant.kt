42c02ee72e1839a43814ec5236bc2e32412e2cfa
kimwest00 <minseohufs@hufs.ac.kr>
2023-09-18 02:32:44 +0900

    refactor: file -> output package 명 변경

--- src/main/kotlin/util/Constant.kt
+++ src/main/kotlin/util/Constant.kt
@@ -1,6 +1,6 @@
 package util
 
-const val originFilePath = "./src/main/kotlin/file/"
+const val originFilePath = "./src/main/kotlin/output/"
 const val compFile = "comp.txt"
 const val idolFile = "idol.txt"
 const val eventFile = "event.txt"
